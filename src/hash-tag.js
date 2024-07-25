d70250d970e7e5a68a1ff3aab229db1eb333bfda
LNSY <lindsey.asterius@gmail.com>
2024-07-25 10:01:53 -0700

    Removed hash-tag

--- src/hash-tag.js
+++
@@ -1,9 +0,0 @@
-
-class HashTag extends HTMLElement {
-  connectedCallback(){
-    const hash_id = this.innerText;
-    this.innerHTML = `<a href="?&file-id=${hash_id}">${hash_id}</a>`
-  }
-}
-
-customElements.define('hash-tag', HashTag);
